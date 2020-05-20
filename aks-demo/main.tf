5b2413f80c9b651f7042b74cd8d3b1743b68b5f7
chrisjeffreyuk <chrisjeffrey@outlook.com>
2020-05-20 09:25:43 +0100

    Updated provider line

--- aks-demo/main.tf
+++ aks-demo/main.tf
@@ -1,3 +1,8 @@
+provider "azurerm" {
+  features {}
+}
+
+
 terraform {
   backend "azurerm" {
     storage_account_name  = "__terraformstorageaccountname__"
@@ -6,15 +11,18 @@ terraform {
   }
 }
 
+
 resource "azurerm_resource_group" "example" {
   name     = "__resourcegroupname__"
   location = "__location__"
 }
 
+
 data "azurerm_key_vault_secret" "clientsecret" {
 name = "clientsecret"
 vault_uri = "https://cjhack-vault.vault.azure.net/"
 }
+
 
 resource "azurerm_kubernetes_cluster" "example" {
   name                = "__k8sclustername__"
@@ -30,10 +38,12 @@ resource "azurerm_kubernetes_cluster" "example" {
     os_disk_size_gb = 30
   }
 
+
   service_principal {
     client_id     = "03fc3f84-61cd-4444-ae6c-401d19ca7145"
     client_secret = "${data.azurerm_key_vault_secret.clientsecret.value}"
   }
+
 
   tags = {
     Environment = "DEMO"
